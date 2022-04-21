f47f99cb3317b91473c5162d5253e4c481164d71
rupalisam1999 <rupalisangale1999@gmail.com>
2022-04-21 06:39:33 +0530

    [Rupali]:Added: Add user in EntityFrameWork

---
+++ BusinessLayer/Servises/UserBL.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Interfases;
+using DataBaseLayer;
+using RepositoryLayer.UserInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Servises
+{
+    public class UserBL : IUserBL
+    {
+        IUserRL userRL;
+        public void AddUser(UserPostModel user)
+        {
+           try
+            {
+                this.userRL.AddUser(user);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ RepositoryLayer/Servises/UserRL.cs
@@ -0,0 +1,38 @@
+﻿using DataBaseLayer;
+using RepositoryLayer.FundooNotesContext;
+using RepositoryLayer.UserInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.UserClass
+{
+    public class UserRL : IUserRL
+    {
+        FundooContext fundoo;
+        public UserRL(FundooContext fundoo)
+        {
+            this.fundoo = fundoo;
+        }
+        public void AddUser(UserPostModel user)
+        {
+            try
+            {
+                Entity.User user1 = new Entity.User();
+                user1.FirstName = user.FirstName;
+                user1.LastName = user.LastName;
+                user1.Email = user.Email;
+                user1.Adress = user.Adress;
+                user1.Password = user.Password;
+                user1.registerdDate = DateTime.Now;
+                fundoo.Users.Add(user1);
+                fundoo.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
